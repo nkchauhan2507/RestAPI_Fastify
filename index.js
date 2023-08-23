a50a3515191ecfe6403309846bfb90e3a7fb6c06
Narayan Chauhan <100989329+nkchauhan2507@users.noreply.github.com>
2023-08-23 06:49:35 +0000

    "initial commit"

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
- 
+require("dotenv").config({"path":`${__dirname}/config.env`})
+const fastify = require('fastify')({});
+const jwt = require('fastify-jwt');
+
+const listenObject = {
+    port: 3000,
+    address: "127.0.0.1",
+};
+fastify.listen(listenObject,async (error, address) => {
+    if(error)
+        console.log(error);
+    else    
+        console.log(`Server is listening on ${address}`);
+});
