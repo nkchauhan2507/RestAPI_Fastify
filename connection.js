89c292417230ce77ab9f220ed3c71f9e652db2fc
Narayan Chauhan <100989329+nkchauhan2507@users.noreply.github.com>
2023-08-24 09:05:47 +0000

    "connection established"

--- connection.js
+++ connection.js
@@ -1,3 +1,32 @@
 const {Client} = require('pg');
 
-function runOnDB(querystr,username,)
+function runOnDB(querystr,username,pass,hostname,portno,dbname){
+    return new Promise(async(resolve,reject)=>{
+        const client = new Client({
+            user:username,
+            host:hostname,
+            password:pass,
+            database:dbname,
+            port:portno
+        })
+        client.connect((err)=>{
+            if(err){
+                return reject(err)
+            }
+        })
+        client.query(querystr,(err,result)=>{
+            client.end()
+            if(err)
+            {
+                return reject(err);
+            }else{
+                if(result)
+                {
+                    return resolve(result);
+                }
+            }
+        })
+    })
+}
+
+module.exports ={runOnDB};
