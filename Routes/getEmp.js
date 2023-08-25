ce25b8619c314ab4037e40e58e9477ea13b65555
Narayan Chauhan <100989329+nkchauhan2507@users.noreply.github.com>
2023-08-25 11:06:54 +0000

    "Unistall nodemon"

--- Routes/getEmp.js
+++ Routes/getEmp.js
@@ -1,19 +1,65 @@
-const {GET_retrieveEmp} = require('../Schema/schema')
-require("dotenv").config({"path":`${__dirname}/config.env`})
-const {GET_getEmp} = require('../URL')
-module.exports = function(fastify,opts,next){
+const { GET_retrieveEmp } = require('../Schema/schema')
+require("dotenv").config({ "path": `${__dirname}/config.env` })
+const { GET_getEmp } = require('../URL')
+const { runOnDB } = require('../connection')
+module.exports = function (fastify, opts, next) {
+
     fastify.route({
-        method:'GET',
-        url:`/${GET_getEmp}/:empid`,
-        schema:GET_retrieveEmp,
-        handler: async(request,response)=>{
-            const {user,password,host,port,databse} = process.env;
-            if(request.params.empid == '*')
-            {
-                //do something
+        method: 'GET',
+        url: `/${GET_getEmp}/:empid`,
+        schema: GET_retrieveEmp,
+        handler: async (request, response) => {
+            const { user, password, host, port, databse } = process.env;
+            if (request.params.empid == '*') {
+                let query = `select firstname,lastname,mobileno,email from "Emp"`;
+                await runOnDB(query, user, password, host, port, databse)
+                    .then((result) => {
+                        if (result.rowCount > 0) {
+                            response.send(result.rows);
+                        } else {
+                            response.code(400).send({
+                                "statusCode": 400,
+                                "error": "Unprocessable Entiry",
+                                "message": "Enterd"
+                            })
+                        }
+                    })
+                    .catch((err) => {
+                        response.send(err);
+                    })
             }
             else {
-                // do something
+                // check weather id exists or not
+                let query = `select id from "Emp" where id = '${request.params.id}`;
+                await runOnDB(query, user, password, host, port, databse)
+                    .then(async (result) => {
+                        if (result.rowCount > 0) {
+                            query = ``;
+                            query = `select firstname,lastname,mobileno,email from "Emp"`;
+                            await runOnDB(query, user, password, host, port, databse)
+                                .then((result) => {
+                                    if(result.rows!=0)
+                                    {
+                                        response.send(result.rows);
+                                    }
+                                })
+                                .catch((err) => {
+                                    response.send(err);
+                                })
+                        } 
+                        else
+                        {
+                            response.code(400).send({
+                                "statusCode": 400,
+                                "error": "Unprocessable Entiry",
+                                "message": "No Rows found with given id."
+                            })
+                        }
+                    })
+                    .catch((err) => {
+                        response.send(err);
+                    })
+
             }
         }
     })
