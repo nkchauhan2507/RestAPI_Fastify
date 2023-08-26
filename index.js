9663560f4bad43c7d1b1cbda4349d6b83462d85d
nkchauhan2507 <nkchauhan785@gmail.com>
2023-08-26 20:31:08 +0530

    "initial commit"

--- index.js
+++ index.js
@@ -1,43 +1,45 @@
 require("dotenv").config({"path":`${__dirname}/config.env`})
 const fastify = require('fastify')({});
 const jwt = require('@fastify/jwt');
-const { readFileSync } = require("fs");
 const path = require('path');
 
 fastify.register(require('@fastify/cors'));
-fastify.register(require('@fastify/swagger'),{
+
+// to load swagger UI
+fastify.register(require('@fastify/static'),{
+    root: path.join(__dirname,'documentation')
+});
+
+fastify.register(require('fastify-swagger'),{
     exposeRoute: true,
+    // routePrefix:'documentation',
     swagger:{
         schemes: ['http'],
         info:{
             title:'Employee API',
-            version: '1.0.0.0'
+            version: '1.0.0.0',
         },
         "tags":[
             {
-                "name":"Employee data related operation"
+                "name":"Employee related operation"
             }
         ],
-        securityDefinitions:{
-            apiKey:{
-                type:'apiKey',
-                name:'Authorization',
-                in:'header'
-            }
-        },
+        // securityDefinitions:{
+        //     apiKey:{
+        //         type:'apiKey',
+        //         name:'Authorization',
+        //         in:'header'
+        //     }
+        // },
         hideUntagged: true
     }
 })
 
-// to load swagger UI
-fastify.register(require('@fastify/static'),{
-    root: path.join(__dirname,'documentation_UI')
-});
+// routes
 
-//routes
-// fastify.register(require('fastify-autoload'),{
-//     dir:path.join(__dirname,'Routes')
-// })
+fastify.register(require('@fastify/autoload'),{
+    dir:path.join(__dirname,'Routes'),
+})
 
 // jwt 
 // fastify.register(require('@fastify/jwt'),{
@@ -88,7 +90,6 @@ fastify.addHook("onRequest",async(req,reply)=>{
 })
 
 
-
 const listenObject = {
     port: 3000,
     address: "127.0.0.1",
@@ -97,5 +98,5 @@ fastify.listen(listenObject,async (error, address) => {
     if(error)
         console.log(error);
     else    
-        console.log(`Server is listening on ${address}`);
+        console.log(`Server is listening on http://localhost:3000/`);
 });
